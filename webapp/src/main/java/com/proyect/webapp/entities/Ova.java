258316d1a7c2d0588e5fbad3e2d3cbb2bd287872
Pascu999 <cristian.pascumal@correounivalle.edu.co>
2022-01-28 22:22:20 -0500

    Ajustes para el rating de Ovas

--- webapp/src/main/java/com/proyect/webapp/entities/Ova.java
+++ webapp/src/main/java/com/proyect/webapp/entities/Ova.java
@@ -69,6 +69,9 @@ public class Ova {
     @Column(name = "link")
     private String ovaLink;
 
+    @Column(name = "rating")
+    private Double  rating ;
+
     @Column(name = "keyS3")
     private String keyS3;
 
